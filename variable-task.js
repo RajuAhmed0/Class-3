07e8c87b9a466aef041ab7bb3c210714ae69eaf4
Your Name <you@example.com>
2024-07-02 13:32:59 +0600

    variable task complate

---
+++ variable-task.js
@@ -0,0 +1,126 @@
+/*  variable - task */ 
+
+/* Declare a variable */
+
+var Fruits;
+var Name = "types of value";
+
+
+/* How can you find out type of a variable */
+
+// Number
+var carPrice = 2000;
+var age = 21;
+
+
+// String 
+
+var FriendsName = "sajib, mehedi, asif"; 
+var carPrice = "400";
+var address = "baraigram, nator"
+
+
+// Boolean
+
+var IsMale = true;
+var IsFemale = false;
+var sajibPlayinFootball = true;
+var sajibMarrideSadia = false;
+
+
+// Undefined
+
+var carName /*carName is undefind*/ = "BMW";
+// console.log(carName);
+
+
+/* Math Operation +, -, *, /, % */
+
+// + math 
+var ApplePrice = 200;
+var OrangePrice = 150;
+
+var FruitsPrice = ApplePrice + OrangePrice;
+// console.log(FruitsPrice);
+
+// - math 
+var MainMoney = 100;
+var BuyGitterPrice = 40;
+
+var Money = MainMoney - BuyGitterPrice;
+// console.log(Money);
+
+// * math 
+var oneApplePrice = 8;
+var AppleBuy = 20;
+
+var TotalMoney = oneApplePrice * AppleBuy;
+// console.log(TotalMoney);
+
+// ÷ math
+ var roast = 30;
+ var person = 10;
+
+ var avarage = roast / person;
+//  console.log(avarage);
+
+// % math 
+var num1 = 200;
+var num2 = 12;
+
+var Total = num1 % num2;
+// console.log(Total);
+
+
+/* Short hand: +=, -=, *=, /=, ++, -- */
+
+// +=short hand
+var sajibAge = 20;
+
+sajibAge += 5;
+// console.log(sajibAge);
+
+// -= short hand 
+
+var sajibGf = 15;
+
+sajibGf -= 8;
+// console.log(sajibGf);
+
+// *= short hand 
+var mahediBuyMango = 20;
+
+mahediBuyMango *= 15;
+// console.log(mahediBuyMango);
+
+
+// /= short hand 
+var mahediBuyMango = 20;
+
+mahediBuyMango /= 5;
+// console.log(mahediBuyMango);
+
+
+// ++ short hand
+var car = 200;
+
+car++;
+// console.log(car);
+
+
+// -- short hand 
+var apple = 50;
+
+apple--;
+console.log(apple);
+
+
+
+
+
+
+
+
+
+
+
