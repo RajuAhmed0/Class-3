7f4e474b8218df5d883c62bb1b0441fb7db66919
Your Name <you@example.com>
2024-07-03 12:04:32 +0600

    array task complate

---
+++ array-task.js
@@ -0,0 +1,10 @@
+/* How to declare an array in JS */
+var FruitsName = ["Apple", "Orange", "Banana", "Watermelon"];
+
+
+/* Number of elements in an Array */
+var FruitsName = ["Apple", "Orange", "Banana", "Watermelon"];
+var lenth = FruitsName.length;
+// FruitsName.length = 2;
+console.log(FruitsName);
+
